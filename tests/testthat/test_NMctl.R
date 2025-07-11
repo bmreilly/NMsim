88045b4246b2269dc67d45c71dba594643c18193
Philip Delff <philip@delff.dk>
2025-07-10 21:03:46 -0400

    simplePath() tests

--- tests/testthat/test_NMctl.R
+++ tests/testthat/test_NMctl.R
@@ -1,5 +1,6 @@
-
-as.NMctl("testData/nonmem/xgxr021.mod")
-as.NMctl.character
-readCtl
-readCtl.character
+if(F){
+    as.NMctl("testData/nonmem/xgxr021.mod")
+    as.NMctl.character
+    readCtl
+    readCtl.character
+}
