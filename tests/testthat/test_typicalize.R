ae6dc4e22a216ee138c4c5065ca1b11f718a2c27
Philip Delff <philip@delff.dk>
2025-08-21 22:30:34 -0400

    typicalize with priors

--- tests/testthat/test_typicalize.R
+++ tests/testthat/test_typicalize.R
@@ -20,47 +20,28 @@ test_that("basic",{
 })
 
 
+test_that("Priors",{
+    fileRef <- "testReference/typicalize_02.rds"
 
-if(FALSE){
-### NMriteInits for typicalize
-##     library(devtools)
-
-    ## load_all("~/wdirs/NMdata",export_all = F)
-    ## load_all("~/wdirs/NMsim",export_all = F)
-
-
-    file.mod="testData/nonmem/xgxr011.mod"
+    ##file.mod="testData/nonmem/xgxr011.mod"
     file.mod="testData/nonmem/xgxr032_sd1_NWPRI.mod"
 
     newfile <- "testOutput/typicalize1.mod"
 
-    res <- NMsim:::typicalize(file.mod)
+    res0 <- NMsim:::typicalize(file.mod)
     
-    inits <- NMreadInits(file.mod,as.fun="data.table",section="all")
-
-    valc.0 <- 1e-30
-    sections.typ <- c("OMEGA","OMEGAP","OMEGAPD")
-    inits[par.type%in%sections.typ,init:=valc.0]
-    inits[par.type%in%sections.typ,FIX:=1]
-
-    inits
-
-    ## load_all("~/wdirs/NMdata",export_all = F)
-    ## load_all("~/wdirs/NMsim",export_all = F)
-
-    inits
-
-
-
-#### not working with OMEGAP. Seems to work with OMEGA and OMEGAPD.
-    ## NMreadSection(file.mod)
-    NMwriteInits(file.mod,inits.tab=inits,update=FALSE,newfile=newfile)
+    secs <- NMreadSection(lines=res0)
     
-    NMwriteInits(file.mod,inits.tab=inits[par.type%in%c("THETA","OMEGA")],update=FALSE,newfile=newfile)
+    res <- secs[c("OMEGA","OMEGAP","OMEGAPD")]
+    expect_equal_to_reference(res,fileRef)
 
+        if(F){
+            res
+        readRDS(fileRef)
+    }
 
-    readLines(newfile)
-}
+    
+})
 
 
 
