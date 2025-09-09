9c16f6cd6b89bbb8a22184bf453f7ec0be99184e
Philip Delff <philip@delff.dk>
2025-09-09 08:20:15 -0400

    test seed

--- tests/testthat/test_sampleCovs.R
+++ tests/testthat/test_sampleCovs.R
@@ -11,7 +11,7 @@ test_that("basic",{
     ## data.covs <- NMscanData(system.file("examples/nonmem/xgxr134.mod",package="NMsim"))
     dos.1 <- NMcreateDoses(TIME=0,AMT=100) 
     data.sim.1 <- NMaddSamples(dos.1,TIME=c(1,4),CMT=2)
-    res <- sampleCovs(data=data.sim.1,Nsubjs=3,col.id.covs="ID",data.covs=data.covs,covs=c("WEIGHTB","eff0"))
+    res <- sampleCovs(data=data.sim.1,Nsubjs=3,col.id.covs="ID",data.covs=data.covs,covs=c("WEIGHTB","eff0"),seed.R=1)
 
     expect_equal_to_reference(res,fileRef)
 })
